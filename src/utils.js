fdd2cf5e0c7b08043a7223446053de642cc26bb2
Melodyn <samelodyn@gmail.com>
2020-11-10 20:59:52 +0300

    fix validator

--- src/utils.js
+++ src/utils.js
@@ -4,16 +4,21 @@ export const validateConfig = (config) => {
   const checkEnv = (expected) => (current, schema) => schema.default(current === expected);
 
   const schema = yup.object().shape({
-    NODE_ENV: yup.string().oneOf(['test', 'local', 'prod']).required(),
+    NODE_ENV: yup.string().oneOf(['test', 'local', 'production']).required(),
     HOST: yup.string().required(),
     PORT: yup.number().required(),
     ROLLBAR_PSI_TOKEN: yup.string().required(),
     IS_TEST_ENV: yup.boolean().when('NODE_ENV', checkEnv('test')),
     IS_LOCAL_ENV: yup.boolean().when('NODE_ENV', checkEnv('local')),
-    IS_PROD_ENV: yup.boolean().when('NODE_ENV', checkEnv('prod')),
+    IS_PROD_ENV: yup.boolean().when('NODE_ENV', checkEnv('production')),
   });
 
-  return schema.validateSync(config, { abortEarly: false });
+  try {
+    schema.validateSync(config, { abortEarly: false });
+  } catch (err) {
+    const errorsString = ['Config validation error:', ...err.errors].join('\n');
+    throw new Error(errorsString);
+  }
 };
 
 export const noop = () => {};
